ab3e632ce4e9a3455bc34e32bd079fe8d8eced9a
Rick <rmackling@outlook.com>
2024-12-07 23:49:44 -0500

    unattended update and rearanged install order

--- debian-shellinit.sh
+++ debian-shellinit.sh
@@ -3,19 +3,19 @@
 # Update apt sources and upgrade
 sudo apt update -y
 
+# Install tmux
+sudo apt install -y tmux
+
 # Install zsh
 sudo apt install -y zsh
 
 # Install Oh My Zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"  --unattended
 
 # Set the theme to 'darkblood'
-sed -i 's/ZSH_THEME=".*"/ZSH_THEME="darkblood"/' ~/.zshrc
-
-# Install tmux
-sudo apt install -y tmux
+sed -i 's/ZSH_THEME=".*"/ZSH_THEME="darkblood"/'
 
 # Apply changes
-source ~/.zshrc
+exec zsg
 
 echo "Installation and configuration complete!"
